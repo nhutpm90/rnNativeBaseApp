c2f7e7d313a067dbd89da2accea940a56a286cae
nhut_pham <nhut.pham@ilogic.vn>
2019-05-10 16:57:11 +0700

    config react-navigation

--- App.js
+++ App.js
@@ -1,45 +1,51 @@
-import React, { Component } from "react";
-import {
-  Container,
-  Header,
-  Title,
-  Content,
-  Footer,
-  FooterTab,
-  Button,
-  Left,
-  Right,
-  Body,
-  Icon,
-  Text
-} from "native-base";
+import React, { Component } from 'react';
+import { Button, View, Text, Image, TouchableOpacity } from 'react-native';
 
-export default class AnatomyExample extends Component {
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+import { Root } from "native-base";
+
+import Header01 from './components/Header01';
+
+const screens = ["Header01"];
+
+class MainScreen extends React.Component {
+  _renderButtons = (screens) => {
+    return screens.map((screen, idx) => {
+      return (
+        <Button key={idx} title={screen} onPress={() => this.props.navigation.navigate(screen)}/>
+      );
+    });
+  }
+
   render() {
     return (
-      <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name="menu" />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Header</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content>
-          <Text>This is Content Section</Text>
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "#95a5a6" }}>
+        <Text>Main</Text>
+        { this._renderButtons(screens) }
+      </View>
     );
   }
 }
+
+const RootStack = createStackNavigator(
+  { 
+    MainScreen: {
+      screen: MainScreen,
+    },
+    Header01: {
+      screen: Header01,
+    },
+  },
+  {
+    initialRouteName: 'MainScreen',
+    headerMode: 'none',
+  }
+)
+
+const AppContainer = createAppContainer(RootStack);
+
+export default () =>
+  <Root>
+    <AppContainer />
+  </Root>;
